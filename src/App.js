8d1a76aea2c7b0732417535497c7f19fb672aba1
Rudin Swagerman <post@rudinswagerman.nl>
2018-04-12 12:01:00 +0200

    updated readme

--- src/App.js
+++ src/App.js
@@ -1,31 +1,23 @@
-import React, { Component } from 'react'
-
+import React from 'react'
 import { Container, Header, Body, Content, Aside, Footer } from './components'
-
-class App extends Component {
-  render() {
-    return (
-      <Container>
-        <Header bg="lightgreen" p={2}>
-          Header
-        </Header>
-        <Body>
-          <Content bg="lightblue" p={2}>
-            Content
-          </Content>
-          <Aside bg="pink" left primary p={2}>
-            Left
-          </Aside>
-          <Aside bg="orange" right p={2}>
-            Right
-          </Aside>
-        </Body>
-        <Footer bg="yellow" p={2}>
-          Footer
-        </Footer>
-      </Container>
-    )
-  }
-}
-
-export default App
+export default () => (
+  <Container>
+    <Header bg="lightgreen" p={2}>
+      Header
+    </Header>
+    <Body>
+      <Content bg="lightblue" p={2}>
+        Content
+      </Content>
+      <Aside bg="pink" left primary p={2}>
+        Left
+      </Aside>
+      <Aside bg="orange" right p={2}>
+        Right
+      </Aside>
+    </Body>
+    <Footer bg="yellow" p={2}>
+      Footer
+    </Footer>
+  </Container>
+)
